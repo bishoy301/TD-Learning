ef9886097da3e5d343e929a7143bfa03e3dab2d0
Bishoy <bishoyrock2@gmail.com>
2017-08-28 11:41:23 -0500

    Code Cleanup and value print

--- Working Memory/td_1d_hrr_wm.cpp
+++ Working Memory/td_1d_hrr_wm.cpp
@@ -10,8 +10,13 @@
 #include "DFTTool.h"
 
 using namespace std;
+using namespace WMtk;
 
 typedef vector<double> HRR;
+
+const int WORLD_SIZE     = 20;
+const int NUMBER_SIGNALS = 3;
+
 
 enum Move {
     Left,
@@ -19,61 +24,59 @@ enum Move {
 };
 
 enum WM {
-    current_working_memory,
-    current_signal,
+	current_working_memory,
+	current_sig,
     nothing
 };
     
+int hrr_size = 1024;
+DFTTool tool(hrr_size);
 
-HRR getState(const DFTTool& tool, HRR world, HRR signal, HRR memory);
+HRR getState(HRR world, HRR signal, HRR memory);
 double getValue(HRR state, HRR& weight);
-Move nextMove(const DFTTool& tool, HRR left, HRR current, HRR right, HRR current_signal, HRR current_memory, HRR weight);
+Move nextMove(HRR left, HRR current, HRR right, HRR current_signal, HRR current_memory, HRR weight);
 Move randMove();
 int getLeft(int current_location);
 int getRight(int current_location);
-WM memoryUpdate(const DFTTool& tool, HRR current_memory, HRR current_signal, HRR nothing_state, HRR current_location_HRR, HRR weight);
+WM memoryUpdate(HRR current_memory, HRR current_signal, HRR nothing_state, HRR current_location_HRR, HRR weight);
 
 int main() {
-    int world_size          = 20;
-    int number_signals      = 3;
-    int number_episodes     = 1000;
-    int steps               = 100;
+    int number_episodes      = 1000;
+    int steps                = 100;
     
-    double default_reward   = 0;
-    double goal_reward      = 1;
-    int rand_goal1          = rand() % (world_size);
-    int rand_goal2          = rand() % (world_size);
+    double default_reward    = 0;
+    double goal_reward       = 1;
+    int rand_goal1           = rand() % (WORLD_SIZE);
+    int rand_goal2           = rand() % (WORLD_SIZE);
     double previous_reward;
     
-    int hrr_size            = 1024;
-    int bias                = 0;
-    double gamma            = 0.9;
-    double learn_rate       = 0.3;
-    double lambda           = 0.5;
+    int bias                 = 0;
+    double gamma             = 0.9;
+    double learn_rate        = 0.3;
+    double lambda            = 0.5;
     double td_error;
 
-    vector<HRR> world(world_size);
-    vector<HRR> signals(number_signals);   // The signals present in environment
-    vector<HRR> memory(number_signals);    // The signals remembered by the agent
+    vector<HRR> world(WORLD_SIZE);
+    vector<HRR> signals(NUMBER_SIGNALS);   // The signals present in environment
+    vector<HRR> memory(NUMBER_SIGNALS);    // The signals remembered by the agent
     HRR weight(hrr_size);
-    vector<HRR> eligibility(hrr_size);
-    int reward[number_signals][world_size];
+    HRR eligibility(hrr_size);
+    int reward[NUMBER_SIGNALS][WORLD_SIZE];
     
-    DFTTool tool(hrr_size);
     
-    for (int i = 0; i < world_size; ++i) {
-        world[i].pushback(tool.hrr());
+    for (int i = 0; i < WORLD_SIZE; ++i) {
+        world[i].push_back(tool.hrr());
     }
 
-    signals[0].pushback(tool.ihrr());
-    memory[0].pushback(tool.ihrr());
-    for (int i = 1; i <= number_signals; ++i) {
-        signals[i].pushback(tool.hrr());
-        memory[i].pushback(tool.hrr());
+    signals[0].push_back(tool.ihrr());
+    memory[0].push_back(tool.ihrr());
+    for (int i = 1; i <= NUMBER_SIGNALS; ++i) {
+        signals[i].push_back(tool.hrr());
+        memory[i].push_back(tool.hrr());
     }
 
-    for (int i = 0; i < number_signals; ++i) {
-        for (int j = 0; j < world_size; ++j) {
+    for (int i = 0; i < NUMBER_SIGNALS; ++i) {
+        for (int j = 0; j < WORLD_SIZE; ++j) {
             reward[i][j] = default_reward;
         }
     }
@@ -82,21 +85,21 @@ int main() {
     reward[1][rand_goal1] = goal_reward;
     reward[2][rand_goal2] = goal_reward;
 
-    for (int episode = 1; episode <= number_episode; ++episode) {
+    for (int episode = 1; episode <= number_episodes; ++episode) {
         /* for (int i = 0; i < hrr_size; ++i) {
-            eligibiltiy[i].pushback(0);
+            eligibiltiy[i].push_back(0);
             } */
         std::fill(eligibility.begin(), eligibility.end(), 0);
 
         int current_task               = (rand() > RAND_MAX/2) ? 1 : 2; 
-        int current_location           = rand() % (world_size);
+        int current_location           = rand() % (WORLD_SIZE);
         HRR current_location_HRR       = world[current_location];
         HRR current_memory             = memory[0];
         HRR current_signal             = signals[current_task];
 
         for (int timestep = 0; timestep < steps; ++timestep) {
-            int current_reward         = reward[current_tast][current_location];
-            HRR current_state          = getState(tool, current_location_HRR, current_signal, current_memory);
+            int current_reward         = reward[current_task][current_location];
+            HRR current_state          = getState(current_location_HRR, current_signal, current_memory);
             double current_value       = getValue(current_state, weight) + bias;
 
             int previous_location      = current_location;
@@ -112,13 +115,13 @@ int main() {
             HRR nothing_state = memory[0];
             
             // Update of Working Memory
-            WM memory_update = memoryUpdate(tool, current_memory, current_signal, nothing_state, current_location_HRR, weight);
+            WM memory_update = memoryUpdate(current_memory, current_signal, nothing_state, current_location_HRR, weight);
 
             // Making the actual memory update
             switch(memory_update) {
                 case current_working_memory:
                     break;                // In the current_memory case we don't need to do anything 
-                case current_signal:
+				case current_sig:
                     current_memory = current_signal;
                     break;
                 case nothing:
@@ -129,7 +132,7 @@ int main() {
                     break;
             }
 
-            current_state = getState(tool, current_location_HRR, current_signal, current_memory);
+            current_state = getState(current_location_HRR, current_signal, current_memory);
             current_value = getValue(current_state, weight) + bias;
 
 
@@ -137,14 +140,14 @@ int main() {
                 td_error = current_value - previous_value;
 
                 for (int i = 0; i < eligibility.size(); ++i) {
-                    eligibility[i] += (previous_state / sqrt(2));
+                    eligibility[i] += (previous_state[i] / sqrt(2));
                 }
 
                 for (int i = 0; i < weight.size(); ++i) {
-                    weight[i] += lrate*eligibility[i]*td_error;
+                    weight[i] += learn_rate*eligibility[i]*td_error;
                 }
 
-                bias += lrate * td_error;
+                bias += learn_rate * td_error;
                 
                 break;
             }
@@ -152,14 +155,14 @@ int main() {
             td_error = current_value - previous_value;
 
             for (int i = 0; i < eligibility.size(); ++i) {
-                eligibility[i] += (previous_state / sqrt(2));
+                eligibility[i] += (previous_state[i] / sqrt(2));
             }
             
             for (int i = 0; i < weight.size(); ++i) {
-                weight[i] += lrate*eligibility[i]*td_error;
+                weight[i] += learn_rate*eligibility[i]*td_error;
             }
             
-            bias += lrate * td_error;
+            bias += learn_rate * td_error;
 
             previous_location = current_location;
             previous_state    = current_state;
@@ -170,10 +173,9 @@ int main() {
             current_signal = memory[0];          // Turning off the signal
 
             // Choosing the next move
-            Move next_move = nextMove(tool,
-                                      world[current_task][getLeft(current_location)],
+            Move next_move = nextMove(world[getLeft(current_location)],
                                       current_state,
-                                      world[current_task][getRight(current_location)],
+                                      world[getRight(current_location)],
                                       current_signal,
                                       current_memory,
                                       weight);
@@ -195,32 +197,39 @@ int main() {
                     break;
             }
 
-            current_location_HRR = world[current_task][current_location];
-            current_state        = getState(tool, current_location_HRR, current_signal, current_memory);
+            current_location_HRR = world[current_location];
+            current_state        = getState(current_location_HRR, current_signal, current_memory);
             current_value        = getValue(current_state, weight) + bias;
 
             td_error = (current_reward + gamma * current_value) - previous_value;
 
             for (int i = 0; i < eligibility.size(); ++i) {
-                eligibility[i] += (previous_state / sqrt(2));
+                eligibility[i] += (previous_state[i] / sqrt(2));
             }
             
             for (int i = 0; i < weight.size(); ++i) {
-                weight[i] += lrate*eligibility[i]*td_error;
+                weight[i] += learn_rate*eligibility[i]*td_error;
             }
             
-            bias += lrate * td_error;
+            bias += learn_rate * td_error;
 
         }
         
     }
 
     // Print the values
+	for (int i = 0; i < NUMBER_SIGNALS; ++i) {
+		for (int j = 0; j < NUMBER_SIGNALS; ++j) {
+			for (int k = 0; k < WORLD_SIZE; ++k) {
+				cout << "Value " << i << " " << j << " " << k << " " << " : " << getValue(getState(world[k], signals[i], memory[j]), weight) << endl;
+			}
+		}
+	}
 
     return 0;
 }
 
-HRR getState(const DFTTool& tool, HRR world, HRR signal, HRR memory) {
+HRR getState(HRR world, HRR signal, HRR memory) {
     return tool.circular_convolution(world, tool.circular_convolution(signal, memory));
 }
 
@@ -235,16 +244,16 @@ double getValue(HRR state, HRR& weight) {
     return sum;
 }
 
-WM memoryUpdate(const DFTTool& tool, HRR current_memory, HRR current_signal, HRR nothing_state, HRR current_location_HRR, HRR weight) {
+WM memoryUpdate(HRR current_memory, HRR current_signal, HRR nothing_state, HRR current_location_HRR, HRR weight) {
     WM update;
-    double current_mem = getValue(getState(tool, current_location_HRR, current_signal, current_memory), weight);
-    dobule current_sig = getValue(getState(tool, current_location_HRR, current_signal, current_signal), weight);
-    double nothin      = getValue(getState(tool, current_locatoin_HRR, current_signal, nothing_state), weight);
+    double current_mem = getValue(getState(current_location_HRR, current_signal, current_memory), weight);
+    double _current_sig = getValue(getState(current_location_HRR, current_signal, current_signal), weight);
+    double nothin      = getValue(getState(current_location_HRR, current_signal, nothing_state), weight);
 
-    if (current_mem >= current_sig && current_mem >= nothin) {
+    if (current_mem >= _current_sig && current_mem >= nothin) {
         update = current_working_memory;
-    } else if (current_sig >= current_mem && current_sig >= nothin) {
-        update = current_signal;
+    } else if (_current_sig >= current_mem && _current_sig >= nothin) {
+		update = current_sig;
     } else {
         update = nothing;
     }
@@ -252,12 +261,12 @@ WM memoryUpdate(const DFTTool& tool, HRR current_memory, HRR current_signal, HRR
     return update;
 }
 
-Move nextMove(const DFTTool& tool, HRR left, HRR current, HRR right, HRR current_signal, HRR current_memory, HRR weight) {
+Move nextMove(HRR left, HRR current, HRR right, HRR current_signal, HRR current_memory, HRR weight) {
     Move movement;
 
-    if (getValue(getState(tool, left, current_signal, current_memory), weight) < getValue(getState(tool, right, current_signal, current_memory), weight)) {
+    if (getValue(getState(left, current_signal, current_memory), weight) < getValue(getState(right, current_signal, current_memory), weight)) {
         movement = Right;
-    } else if (getValue(getState(tool, right, current_signal, current_memory), weight) < getValue(getState(tool, left, current_signal, current_memory), weight)) {
+    } else if (getValue(getState(right, current_signal, current_memory), weight) < getValue(getState(left, current_signal, current_memory), weight)) {
         movement = Left;
     } else {
         randMove();
@@ -281,21 +290,21 @@ int getLeft(int current_location) {
     int left;
 
     if (current_location == 0) {
-        left = world_size - 1;
+        left = WORLD_SIZE - 1;
     } else {
-        left = current_locatoin - 1;
+        left = current_location - 1;
     }
 
     return left;
 }
 
-int getRight(int current_location); {
+int getRight(int current_location) {
     int right;
 
-    if (current_location == world_size - 1) {
+    if (current_location == WORLD_SIZE - 1) {
         right = 0;
     } else {
-        right = current_locatoin + 1;
+        right = current_location + 1;
     }
 
     return right;
