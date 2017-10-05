a95d65c110d63e12131a0690ff7828db723576c2
Bishoy Boktor <bishoy.boktorsd@mtsu.edu>
2017-10-04 20:16:20 -0500

    Initializing Envrionment Variable

--- attention_filter.py
+++ attention_filter.py
@@ -1,1 +1,23 @@
+from random import randrange, randint
 import hrr
+import numpy as np
+
+
+bias = 1
+gamma = 0.9
+lrate = 0.3
+td_lambda = 0.5
+worldSize = 20
+redGoal = randrange(0, worldSize)
+greenGoal = randrange(0, worldSize)
+lengthHRR = 1024
+signalNumber = 3
+defaultReward = 0
+
+world       = np.zeros([worldSize, lengthHRR])
+signals     = np.zeros([signalNumber, lengthHRR])
+eligibility = np.zeros(lengthHRR)
+reward      = np.zeros(lengthHRR)
+
+weights = hrr.hrr(lengthHRR)
+
